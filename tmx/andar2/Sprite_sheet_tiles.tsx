48bd610c168b995a33545f343270f325858e1c42
Igor Mello <igor.oliveira.mello@gmail.com>
2025-12-24 02:02:51 -0300

    Initial Commit

---
+++ tmx/andar2/Sprite_sheet_tiles.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Sprite_sheet_tiles" tilewidth="32" tileheight="32" tilecount="77" columns="11">
+ <image source="../../assets/TileSet/tileset_andar2.png" width="352" height="224"/>
+ <tile id="0">
+  <properties>
+   <property name="tipo_colisao" value="parede"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.363636" y="8.54545" width="32.5455" height="10.5455"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="tipo_colisao" value="parede"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="20.9091" y="-0.181818" width="10.9091" height="32.3636"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="tipo_colisao" value="parede"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.363636" y="9.09091" width="10.5455" height="22.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="tipo_colisao" value="obstaculo"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="4" width="23.9475" height="12.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="tipo_colisao" value="obstaculo"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="4.02222" width="23.9778" height="12.9333"/>
+  </objectgroup>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="tipo_colisao" value="obstaculo"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9.9375" y="0" width="12" height="12.4375"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="tipo_colisao" value="obstaculo"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10" y="0" width="11.875" height="8.125"/>
+   <object id="2" x="2.125" y="5" width="27.75" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="tipo_colisao" value="parede"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="22.7273"/>
+  </objectgroup>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="tipo_colisao" value="parede"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.181818" y="0" width="11.0909" height="23.2727"/>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="tipo_colisao" value="obstaculo"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5" y="0.125" width="22.875" height="8.875"/>
+  </objectgroup>
+ </tile>
+</tileset>
